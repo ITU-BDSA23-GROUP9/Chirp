119f1073afca69f5f208b378f70f355ae5e52ea2
JohanSandager <Johan@sandagerbennedsen.dk>
2023-09-05 13:18:31 +0200

    Add cheep option with write to CSV file
    Co-authored-by: Oline <okre@itu.dk>, Clara <clwj@itu.dk>, Anton <anlf@itu.dk>, Jonas <kram@itu.dk> & Lauritz <lana@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -3,21 +3,33 @@ using Microsoft.VisualBasic.FileIO;
 
 var path = "chirp_cli_db.csv";
 
-using TextFieldParser parser = new TextFieldParser(path);
-parser.TextFieldType = FieldType.Delimited;
-parser.SetDelimiters(",");
-
-parser.ReadFields();
-
-while(!parser.EndOfData)
+if(args[0] == "read")
 {
-    var currentRow = parser.ReadFields();
-    var author = currentRow[0];
-    var message = currentRow[1];
-    var timestamp = currentRow[2];
-    var dateTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(timestamp)).LocalDateTime;
-    var formattedDate = dateTime.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture); 
-    var formattedCheep = $"{author} @ {formattedDate}: {message}";
+    using TextFieldParser parser = new TextFieldParser(path);
+    parser.TextFieldType = FieldType.Delimited;
+    parser.SetDelimiters(",");
 
-    Console.WriteLine(formattedCheep);
+    parser.ReadFields();
+
+    while(!parser.EndOfData)
+    {
+        var currentRow = parser.ReadFields();
+        var author = currentRow[0];
+        var message = currentRow[1];
+        var timestamp = currentRow[2];
+        var dateTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(timestamp)).LocalDateTime;
+        var formattedDate = dateTime.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture);
+        var formattedCheep = $"{author} @ {formattedDate}: {message}";
+
+        Console.WriteLine(formattedCheep);
+    }
+}
+else if(args[0] == "cheep")
+{
+    using StreamWriter writer = new StreamWriter(path, true);
+    var userName = Environment.UserName;
+    var currentTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+    var message = args[1];
+    var saveFormattedCheep = $"{userName},\"{message}\",{currentTimestamp}";
+    writer.WriteLine(saveFormattedCheep);
 }
