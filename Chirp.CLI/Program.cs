8ddb2560e775d33b8cf5d7c75dd929d885cc2a61
olinesk <okre@itu.dk>
2023-09-12 15:50:59 +0200

    implement Docopt in Program.cs
    Co-authored-by: Anton <anlf@itu.dk>
    
    Co-authored-by: Lauritz <lana@itu.dk>
    
    Co-authored-by: Jonas <kram@itu.dk>
    
    Co-authored-by: Johan <jsbe@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -2,21 +2,28 @@
 using CsvHelper;
 using SimpleDB;
 using UI;
+using DocoptNet;
 
+IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
 
-Cheep createCheep(string message) {
+Cheep CreateCheep(string message) {
     var userName = Environment.UserName;
     var currentTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
     return new Cheep(userName, message, currentTimestamp);
 }
 
+const string usage = @"
+Usage:
+    dotnet run read
+    dotnet run cheep <message>
+";
+var arguments = new Docopt().Apply(usage, args, exit: true);
 
-IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
-if(args[0] == "read")
+if (arguments["read"].IsTrue)
 {
     UserInterface.PrintCheeps(database.Read());
 }
 else if(args[0] == "cheep")
 {
-    database.Store(createCheep(args[1]));
+    database.Store(CreateCheep(args[1]));
 }
