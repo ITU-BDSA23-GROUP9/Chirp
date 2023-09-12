de806e21d476f4503817815e76b6240d2e4c41f6
AntonFriis <anlf@itu.dk>
2023-09-12 18:19:50 +0200

    fix issues with docopt

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,29 +1,30 @@
-﻿using System.Globalization;
-using CsvHelper;
-using SimpleDB;
+﻿using SimpleDB;
 using UI;
 using DocoptNet;
 
 IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
 
-Cheep CreateCheep(string message) {
+static Cheep CreateCheep(string message) {
     var userName = Environment.UserName;
     var currentTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
     return new Cheep(userName, message, currentTimestamp);
 }
 
-const string usage = @"
+const string usage = @"Chirp.
+
 Usage:
-    dotnet run read
-    dotnet run cheep <message>
+    Chirp.CLI.dll read
+    Chirp.CLI.dll cheep <message>
 ";
-var arguments = new Docopt().Apply(usage, args, exit: true);
+
+var arguments = new Docopt().Apply(usage, args)!;
 
 if (arguments["read"].IsTrue)
 {
     UserInterface.PrintCheeps(database.Read());
 }
-else if(args[0] == "cheep")
+else if (arguments["cheep"].IsTrue)
 {
-    database.Store(CreateCheep(args[1]));
+    var message = arguments["<message>"].ToString();
+    database.Store(CreateCheep(message));
 }
