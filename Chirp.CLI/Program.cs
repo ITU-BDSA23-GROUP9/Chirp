9f28c8590dcbe03d267ec0f3a533c1315caebd52
Jonas Kramer <kram@itu.dk>
2023-09-12 14:54:06 +0200

    Move generic store method from program.cs to CSVDatabase.
    Co-authored-by: Anton <anlf@itu.dk>
    
    Co-authored-by: Oline <okre@itu.dk>
    
    Co-authored-by: Lauritz <lana@itu.dk>
    
    Co-authored-by: Johan <jsbe@itu.dk>
    
    Co-authored-by: Clara <clwj@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -7,6 +7,11 @@ string formatDateTime(long timestamp)
     return dateTime.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture);
 }
 
+Cheep createCheep(string message) {
+    var userName = Environment.UserName;
+    var currentTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+    return new Cheep(userName, message, currentTimestamp);
+}
 void readCheep(Cheep cheep)
 {
     var formattedDate = formatDateTime(cheep.Timestamp);
@@ -15,17 +20,6 @@ void readCheep(Cheep cheep)
     Console.WriteLine(formattedCheep);
 }
 
-void writeCheep(string message)
-{
-    using StreamWriter writer = new("chirp_cli_db.csv", true);
-    using CsvWriter csvWriter = new(writer, CultureInfo.InvariantCulture);
-
-    var userName = Environment.UserName;
-    var currentTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
-    var saveCheep = new Cheep(userName, message, currentTimestamp);
-    csvWriter.WriteRecord(saveCheep);
-    writer.WriteLine();
-}
 
 IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
 if(args[0] == "read")
@@ -37,5 +31,5 @@ if(args[0] == "read")
 }
 else if(args[0] == "cheep")
 {
-    writeCheep(args[1]);
+    database.Store(createCheep(args[1]));
 }
