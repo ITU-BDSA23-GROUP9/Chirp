133ec53127fa1c554342122090866ae5fc53f7f9
Jonas Kramer <kram@itu.dk>
2023-09-12 15:21:16 +0200

    Fix errors.
    Co-authored-by: Anton <anlf@itu.dk>
    
    Co-authored-by: Oline <okre@itu.dk>
    
    Co-authored-by: Lauritz <lana@itu.dk>
    
    Co-authored-by: Johan <jsbe@itu.dk>
    
    Co-authored-by: Clara <clwj@itu.dk>

--- Chirp.CLI/UserInterface.cs
+++ Chirp.CLI/UserInterface.cs
@@ -1,6 +1,6 @@
 namespace UI;
-
-public static class UserInterface () 
+using System.Globalization;
+public static class UserInterface 
 {
     static string FormatDateTime(long timestamp)
     {
@@ -9,16 +9,16 @@ public static class UserInterface ()
     }
     static void ReadCheep(Cheep cheep)
     {
-        var formattedDate = formatDateTime(cheep.Timestamp);
+        var formattedDate = FormatDateTime(cheep.Timestamp);
         var formattedCheep = $"{cheep.Author} @ {formattedDate}: {cheep.Message}";
 
         Console.WriteLine(formattedCheep);
     }
-    static void PrintCheeps (IEnumerable<Cheep> cheeps) 
+    public static void PrintCheeps (IEnumerable<Cheep> cheeps) 
     {
         foreach (var cheep in cheeps)
         {
-            readCheep(cheep);
+            ReadCheep(cheep);
         }
     }
 }
