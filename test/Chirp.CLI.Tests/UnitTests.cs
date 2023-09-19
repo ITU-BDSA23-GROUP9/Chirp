d4244a369bcd98bbbe7b8fc201eac31e49b66433
AntonFriis <anlf@itu.dk>
2023-09-19 11:59:59 +0200

    add unittest for Cheep Record

--- test/Chirp.CLI.Tests/UnitTests.cs
+++ test/Chirp.CLI.Tests/UnitTests.cs
@@ -1,10 +1,23 @@
+
 namespace Chirp.CLI.Tests;
 
 public class UnitTests
 {
     [Fact]
-    public void Test1()
+    public void TestCheepRecord()
     {
-
+        // Arrange
+        var cheep = new Cheep("lana", "hello world", 1695117316);
+        // Act
+        var name = cheep.Author;
+        var message = cheep.Message;
+        var timestamp = cheep.Timestamp;
+        // Assert
+        Assert.Equal("lana", name);
+        Assert.Equal("hello world", message);
+        Assert.Equal(1695117316, timestamp);
     }
+
+    [Fact]
+    publi
 }
