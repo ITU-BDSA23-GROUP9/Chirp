b919beb72d6c7a2ca987eefc6d86ed356d8e892c
Lauritz Andersen <lana@itu.dk>
2023-09-19 13:40:19 +0200

    Add integration tests for UI PrintCheeps.
    Co-authored-by: Anton <anlf@itu.dk>
    
    Co-authored-by: Johan <jsbe@itu.dk>
    
    Co-authored-by: Jonas <kram@itu.dk>

--- test/Chirp.CLI.Tests/IntegrationTests.cs
+++ test/Chirp.CLI.Tests/IntegrationTests.cs
@@ -1,10 +1,35 @@
+using SimpleDB;
+using UI;
+
 namespace Chirp.CLI.Tests;
 
 public class IntegrationTests
 {
-    [Fact]
-    public void Test1()
+    private CSVDatabase<Cheep> SetupDatabase()
     {
+        return new CSVDatabase<Cheep>("../../../../testdata/testdatabase.csv");
+    }
+
+    [Fact]
+    public void UIPrintCheepsIntegrationTest()
+    {
+        // Arrange
+        var databasecheeps = SetupDatabase().Read();
+
+        var formattedCheeps =
+        "ropf @ 08/01/23 14:09:20: Hello, BDSA students!\nrnie @ 08/02/23 14:19:38: Welcome to the course!\nrnie @ 08/02/23 14:37:38: I hope you had a good summer.\nropf @ 08/02/23 15:04:47: Cheeping cheeps on Chirp :)";
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        //Act
+        UserInterface.PrintCheeps(databasecheeps);
+
+        //Assert
+        Assert.Equal(formattedCheeps, output.ToString().Trim());
+
+
+
+
 
     }
 }
