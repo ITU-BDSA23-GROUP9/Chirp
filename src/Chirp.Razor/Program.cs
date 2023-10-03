f2be037bbf80b79b981a73c884597ce3dc59ad1f
ClaraWJ <clwj@itu.dk>
2023-10-03 12:39:57 +0200

    add integration-test for the razor-pages-application
    Co-authored-by: Anton <anlf@itu.dk>
    
    Co-authored-by: Oline <okre@itu.dk>

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +25,5 @@ app.UseRouting();
 app.MapRazorPages();
 
 app.Run();
+
+public partial class Program{}
