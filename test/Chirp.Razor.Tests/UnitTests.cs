ecfa3dd12add33ea13b2139dc431ca4ac93e86de
JohanSandager <jsbe@itu.dk>
2023-10-24 11:25:48 +0200

    Removing archaic method
    Co-authored-by: Anton <anlf@itu.dk>
    
    Co-authored-by: Oline <okre@itu.dk>
    
    Co-authored-by: Clara <clwj@itu.dk>

--- test/Chirp.Razor.Tests/UnitTests.cs
+++ test/Chirp.Razor.Tests/UnitTests.cs
@@ -27,4 +27,28 @@ public class UnitTests
         Assert.Equal("Hej, velkommen til kurset.", result[0].message);
         Assert.Equal("2023-08-01 13:08:28", result[0].timestamp);
     }
+
+    [Fact]
+    public async void FindAuthorByNameTest()
+    {
+        // Arrange
+        using var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<ChirpContext>().UseSqlite(connection);
+        using var context = new ChirpContext(builder.Options);
+        await context.Database.EnsureCreatedAsync();
+        var repository = new CheepRepository(context);
+        var authorGuid = Guid.NewGuid();
+        context.Cheeps.Add(new Cheep() { CheepId = 1, AuthorId = authorGuid, Author = new Author() { AuthorId = authorGuid, Name = "Anton", Email = "anlf@itu.dk" }, Text = "Hej, velkommen til kurset.", TimeStamp = DateTime.Parse("2023-08-01 13:08:28") });
+        context.SaveChanges();
+
+        // Act
+        var result = await repository.FindAuthorByName("Anton");
+        var authorCheepsList = result.Cheeps;
+
+        // Assert
+        Assert.Equal("Anton", result.Name);
+        Assert.Equal("anlf@itu.dk", result.Email);
+        Assert.Single(authorCheepsList);
+    }
 }
