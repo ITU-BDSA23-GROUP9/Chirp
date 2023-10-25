89a111ded77043fd4faad2f0521743d34dcef75c
JohanSandager <jsbe@itu.dk>
2023-10-25 12:11:50 +0200

    Changing GetCheepsFromAuthor from taking string author to AuthorDTO

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -1,7 +1,7 @@
 public interface ICheepRepository
 {
     public Task<List<CheepDTO>> GetCheeps(int limit, int pageNumber);
-    public Task<List<CheepDTO>> GetCheepsFromAuthor(string author, int limit, int pageNumber);
+    public Task<List<CheepDTO>> GetCheepsFromAuthor(AuthorDTO author, int limit, int pageNumber);
     public Task<List<CheepDTO>> GetAllCheeps();
     public Task<int> GetTotalCheepCount();
     public Task<int> GetTotalCheepCountFromAuthor(string author);
