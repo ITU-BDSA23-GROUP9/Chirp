d541863f54df0518027ac5d703940f9cdf6c3f05
Lauritz Andersen <lauritzandersen@LRTZ-2.local>
2023-11-20 14:18:58 +0100

    Add cheep form on Public timeline
    Co-authored-by: Jonas <kram@itu.dk>
    
    Co-authored-by: Clara <clwj@itu.dk>

--- src/Chirp.Web/Pages/Cheep.cshtml.cs
+++ src/Chirp.Web/Pages/Cheep.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+namespace Chirp.Web.Pages
 {
     public class CheepModel : PageModel
     {
+        private readonly ICheepRepository _repository; 
+
+        [BindProperty]
+        public Cheep cheep { get; set; }
         public void OnGet()
         {
         }
+
+        /*
+        public async Task<IActionResult> OnPost() {
+            var cheepToPost = new CheepDTO(cheep.Text, cheep.Author.ToString(), cheep.TimeStamp.ToString())
+
+            await _repository.CreateCheep(cheepToPost);
+
+            return RedirectToPage("/Profile"); //Go to profile after posting a cheep
+        }
+        */
     }
 }
