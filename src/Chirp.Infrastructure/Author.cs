bdb046c173a8b5c9ef515e1e016c4b3d057f80fe
lanaitu <lana@itu.dk>
2023-11-21 14:09:25 +0100

    Add followers and following to Author.cs

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -3,4 +3,7 @@ using Microsoft.AspNetCore.Identity;
 public class Author : IdentityUser
 {
     public List<Cheep> Cheeps { get; set; } = new();
+    public ICollection<Author>? Followers { get; set; } 
+    public ICollection<Author>? Following { get; set; }
+    
 }
