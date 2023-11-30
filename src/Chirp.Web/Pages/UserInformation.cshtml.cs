4c296a4c6dddf33bb9e0d25793b7230cf0f319ab
olinesk <okre@itu.dk>
2023-11-30 15:20:17 +0100

    Add forget user functionality
    Co-authored-by: Anton <anlf@itu.dk>
    
    Co-authored-by: Johan <jsbe@itu.dk>
    
    Co-authored-by: Jonas <kram@itu.dk>
    
    Co-authored-by: Clara <clwj@itu.dk>
    
    Co-authored-by: Lauritz <lana@itu.dk>

--- src/Chirp.Web/Pages/UserInformation.cshtml.cs
+++ src/Chirp.Web/Pages/UserInformation.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,23 @@ namespace MyApp.Namespace
 {
     public class UserInformationModel : PageModel
     {
+        private readonly UserManager<Author> _userManager;
+        private readonly SignInManager<Author> _signInManager;
+        public UserInformationModel(UserManager<Author> userManager, SignInManager<Author> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostForgetUser()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            await _userManager.DeleteAsync(user);
+            await _signInManager.SignOutAsync();
+            return LocalRedirect(Url.Content("~/"));
         }
     }
 }
